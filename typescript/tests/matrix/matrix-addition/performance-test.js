2100f5d9837dd06077de9eea43d87fcf53bd904f
unknown <yousif.khalil.abdulkarim.1999@gmail.com>
2024-04-13 17:08:04 +0200

    added test automation scripts for performance benchmarks

---
+++ typescript/tests/matrix/matrix-addition/performance-test.js
@@ -0,0 +1,41 @@
+// @ts-check
+import { resolve } from "node:path";
+import {
+  excuteWithLogging,
+  getSystemInfo,
+  prettifyObjectKeys,
+  withCsv,
+} from "../../cpu-test-helper.js";
+import { readdirSync } from "node:fs";
+
+const fileNames = readdirSync("./tests/matrix/matrix-addition")
+  .filter((file) => file.endsWith(".html"))
+  .map((file) => file.split(".")[0]);
+
+for (const fileName of fileNames.slice(0, 1)) {
+  await withCsv(fileName, async (csv) => {
+    /**
+     * @type{Omit<import("../../cpu-test-helper.js").IExcuteOptions, "browser">}
+     */
+    const settings = {
+      filePath: resolve(`./tests/matrix/matrix-addition/${fileName}.html`),
+      iterationAmount: 2,
+      async onPerformanceMessage(performanceMessage) {
+        csv.write(
+          prettifyObjectKeys({
+            ...getSystemInfo(),
+            ...performanceMessage,
+          })
+        );
+      },
+    };
+    await excuteWithLogging({
+      browser: "chrome",
+      ...settings,
+    });
+    await excuteWithLogging({
+      browser: "firefox",
+      ...settings,
+    });
+  });
+}
